126c5d47fedc7d5c45c46a733702ddc5d5c48194
Utkarsh Chourasia <5.utkarshchourasia@gmail.com>
2025-04-10 11:54:54 +0530

    Update Workflows
    Signed-off-by: Utkarsh Chourasia <5.utkarshchourasia@gmail.com>

--- dagger/.dagger/src/index.ts
+++ dagger/.dagger/src/index.ts
@@ -1,19 +1,35 @@
-/**
- * A generated module for Dagger functions
- *
- * This module has been generated via dagger init and serves as a reference to
- * basic module structure as you get started with Dagger.
- *
- * Two functions have been pre-created. You can modify, delete, or add to them,
- * as needed. They demonstrate usage of arguments and return types using simple
- * echo and grep commands. The functions can be called from the dagger CLI or
- * from one of the SDKs.
- *
- * The first line in this comment block is a short description line and the
- * rest is a long description with more detail on the module's purpose or usage,
- * if appropriate. All modules should have a short description.
- */
-import { Container, dag, Directory, File, func, object, Socket } from "@dagger.io/dagger"
+import { argument, Container, dag, Directory, File, func, object, Secret, Socket } from "@dagger.io/dagger"
+
+@object()
+class GoProject {
+  source: Directory
+  ctr: Container
+
+  constructor(
+    @argument({ defaultPath: "/", ignore: ["!**/*.go", "!go.sum", "!go.mod"] })
+    source: Directory
+  ) {
+    this.source = source
+    this.ctr = dag.container()
+  }
+
+  @func()
+  container(version: string = "1.24"): GoProject {
+    this.ctr = this.ctr.from(`docker.io/library/golang:${version}`)
+      .withDirectory("/go/src", this.source)
+      .withWorkdir("/go/src")
+      .withExec(["go", "mod", "download"])
+    return this
+  }
+
+  @func()
+  build(output: string, mainFile: string): File {
+    return this.ctr
+      .withMountedCache("/root/.cache/go-build", dag.cacheVolume("go-build-cache"))
+      .withExec(["go", "build", "-o", output, mainFile])
+      .file(`/go/src/${output}`)
+  }
+}
 
 @object()
 export class Dagger {
@@ -25,50 +41,29 @@ export class Dagger {
   }
 
   @func()
-  setupGoContainer(source: Directory): Container {
-    return dag.container().from("docker.io/library/golang:1.24")
-      .withDirectory("/go/src", source, { include: ["go.mod", "go.sum"] })
-      .withWorkdir("/go/src")
-      .withExec(["go", "mod", "download"])
-  }
-
-  /**
-  * Has API and QuotaCheck sub-commands
-  */
-  @func()
   buildUnkeyAPI(source: Directory): File {
-    return this.setupGoContainer(source)
-      .withDirectory("/go/src", source, { exclude: ["go.mod", "go.sum"] })
-      .withMountedCache("/root/.cache/go-build", dag.cacheVolume("go-build-cache"))
-      .withExec(["go", "build", "-o", "unkey-api", "./build/api/main.go"])
-      .file("/go/src/unkey-api")
+    const project = new GoProject(source)
+    return project.container().build("unkey-api", "./build/api/main.go")
   }
 
-  /**
-  * Has Health and QuotaCheck sub-commands
-  */
   @func()
   buildUnkeyHealth(source: Directory): File {
-    return this.setupGoContainer(source)
-      .withDirectory("/go/src", source, { exclude: ["go.mod", "go.sum"] })
-      .withMountedCache("/root/.cache/go-build", dag.cacheVolume("go-build-cache"))
-      .withExec(["go", "build", "-o", "unkey-health", "./build/health/main.go"])
-      .file("/go/src/unkey-health")
+    const project = new GoProject(source)
+    return project.container().build("unkey-health", "./build/health/main.go")
   }
 
   @func()
-  release(gitdir: Directory, dockersocket: Socket): Directory {
-    dag.container()
+  async release(gitdir: Directory, dockersocket: Socket, ghtoken: string, dockerUser: string, dockerSecret: Secret): Promise<Directory> {
+    var plainTxt = await dockerSecret.plaintext()
     return dag.goreleaser()
       .withGoCache() // Enable Caching for Go Builds
       .withSource(gitdir) // to get info related to git tags
-
       .ctr() // Returning Container to mount Docker
-
       .withWorkdir("/mnt/go") // go.mod and goreleaser YAML is `go` dir
       .withUnixSocket("/var/run/docker.sock", dockersocket)
-
-      .withExec(["goreleaser", "release", "--snapshot"])
+      .withEnvVariable("GITHUB_TOKEN", ghtoken) // Required by goreleaser, in case a package needs to be published
+      .withExec(["docker", "login", "-u", dockerUser, "--password-stdin"], { stdin: plainTxt })
+      .withExec(["goreleaser", "release", "--clean"])
       .directory("/mnt/go/dist") // Export the Binary Packages
   }
 }
